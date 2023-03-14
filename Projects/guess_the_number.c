e385dd48c3f26d44963c5965669996d21dd6153f
Swapnil Nanda <126653343+swapnil312@users.noreply.github.com>
2023-03-14 07:01:30 +0530

    Update guess_the_number.c

--- Projects/guess_the_number.c
+++ Projects/guess_the_number.c
@@ -1,4 +1,6 @@
-// We will write a program that generates a random number and asks the player to guess it. If the player's guess is higher than the actual numbe, the program displays "Lower number plaese", Similarly if the user's guess is too low the program prints "Higher number please". When the user guesses the correct answer, the program displays the number of guesses the player used to arrive at the number.
+// We will write a program that generates a random number and asks the player to guess it. If the player's guess is higher than the actual number, 
+// the program displays "Lower number plaese", Similarly if the user's guess is too low the program prints "Higher number please". 
+// When the user guesses the correct answer, the program displays the number of guesses the player used to arrive at the number.
 
 # include <stdio.h>
 # include <stdlib.h>
